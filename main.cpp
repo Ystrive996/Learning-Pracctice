5a47fe8aeb0d01314273411421a626f78e285e36
Ystrive996 <1731118576@qq.com>
2025-10-23 21:23:40 +0800

    Add files via upload
    水仙花数

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num = 100;
+	do
+	{
+		//cout << num << endl;
+		int a = num % 10;
+		int b = num / 10 % 10;
+		int c = num / 100;
+		if (a * a * a + b * b * b + c * c * c == num)
+		{
+			cout << num << " is a narcissistic number." << endl;
+		}
+		
+		num++;
+	} while (num < 1000);
+}
